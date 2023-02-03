f9d6d2588577f9abed6845f86c6da62dc8b0ce30
GUSTAVO ANTONIOLI MEDEIROS <94537799+GustaAntonioli@users.noreply.github.com>
2023-02-03 13:26:55 -0300

    .

---
+++ soma.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/* 1. Faça um programa que receba dois números, 
+      calcule e mostre a subtração 
+      do primeiro número pelo segundo. */
+
+public class soma {
+    
+    public static void main(String[] args){ 
+        try (Scanner scan = new Scanner(System.in)){
+
+            int a ;
+            int b ;
+            int resultado = (a-b);
+                
+                                
+                    System.out.print("Digite o primeiro número: ");
+                    scan.nextInt();
+              
+                    System.out.print("Digite o segundo número: ");
+                    scan.nextInt();
+              
+                    resultado = a - b;
+                    System.out.println("Resultado da subtração: " + resultado);
+                }
+                
+        }
+    }
